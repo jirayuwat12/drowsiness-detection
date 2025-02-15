1379cb50a3fc67517cf73e795dfe287465fdafb2
jirayuwat12 <jirayuwat12@gmail.com>
2025-02-15 23:23:59 +0700

    Add FaceDetector class for face detection functionality

---
+++ drowsiness_detection/face_detection/face_detector.py
@@ -0,0 +1,18 @@
+from drowsiness_detection.dataclasses.drowsiness_image import DrowsinessImage
+
+
+class FaceDetector:
+    def __init__(self) -> None:
+        pass
+
+    # TODO: Implement the detect method
+    def detect(self, drowsiness_image: DrowsinessImage) -> DrowsinessImage:
+        """
+        Detects the face in the image
+
+        :param drowsiness_image: The image to detect the face.
+        :type drowsiness_image: DrowsinessImage
+        :return: The image with the face detected.
+        :rtype: DrowsinessImage
+        """
+        pass
