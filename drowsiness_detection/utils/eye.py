28d4331d7c51032dbcb9c1a8679f43500649025b
jirayuwat12 <jirayuwat12@gmail.com>
2025-02-22 23:19:35 +0700

    Refactor compute_eye_aspect_ratio to accept eye position list and remove Eye class dependency

--- drowsiness_detection/utils/eye.py
+++ drowsiness_detection/utils/eye.py
@@ -1,25 +1,20 @@
 from drowsiness_detection.dataclasses import Eye
+from drowsiness_detection.dataclasses.position import Position
 
 
-def compute_eye_aspect_ratio(eye: Eye) -> float:
+def compute_eye_aspect_ratio(eye_position: list[Position]) -> float:
     """
     Compute the eye aspect ratio (EAR) for a given eye landmark. The EAR is a measure of how open the eye is.
 
-    :param eye: The eye landmark to compute the EAR for.
-    :type eye: Eye
+    :param eye_position: The eye landmark positions.
+    :type eye_position: list[Position]
     :return: The Eye Aspect Ratio (EAR) for the given eye landmark.
     :rtype: float
     :raises ValueError: If the eye landmark is not an instance of Eye.
 
     Ref: https://medium.com/analytics-vidhya/eye-aspect-ratio-ear-and-drowsiness-detector-using-dlib-a0b2c292d706
     """
-
-    if not isinstance(eye, Eye):
-        raise ValueError("The eye landmark must be an instance of Eye.")
-
-    antecendents = abs(eye.left_eye_positions[1] - eye.left_eye_positions[5]) + abs(
-        eye.left_eye_positions[2] - eye.left_eye_positions[4]
-    )
-    consequents = 2 * abs(eye.left_eye_positions[0] - eye.left_eye_positions[3])
+    antecendents = abs(eye_position[1] - eye_position[5]) + abs(eye_position[2] - eye_position[4])
+    consequents = 2 * abs(eye_position[0] - eye_position[3])
     ear = antecendents / consequents
     return ear
