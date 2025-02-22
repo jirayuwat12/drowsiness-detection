58ed2a59cc93a29f0091e868a89252c1139eb655
jirayuwat12 <jirayuwat12@gmail.com>
2025-02-22 23:19:13 +0700

    Add inplace parameter to detect methods in BaseEyeDetector and BaseFaceDetector

--- drowsiness_detection/eye_detection/base_eye_detector.py
+++ drowsiness_detection/eye_detection/base_eye_detector.py
@@ -8,12 +8,14 @@ class BaseEyeDetector(ABC):
         pass
 
     @abstractmethod
-    def detect(self, face: Face) -> Face:
+    def detect(self, face: Face, inplace: bool = False) -> Face:
         """
         Detects the eyes in the face
 
         :param face: The face to detect the eyes.
         :type face: Face
+        :param inplace: If True, the face will be modified in place. Otherwise, a new face will be created.
+        :type inplace: bool
         :return: The face with the eyes detected.
         :rtype: Face
         """
--- drowsiness_detection/face_detection/base_face_detector.py
+++ drowsiness_detection/face_detection/base_face_detector.py
@@ -8,12 +8,14 @@ class BaseFaceDetector(ABC):
         pass
 
     @abstractmethod
-    def detect(self, drowsiness_image: DrowsinessImage) -> DrowsinessImage:
+    def detect(self, drowsiness_image: DrowsinessImage, inplace: bool = False) -> DrowsinessImage:
         """
         Detects the face in the image
 
         :param drowsiness_image: The image to detect the face.
         :type drowsiness_image: DrowsinessImage
+        :param inplace: If True, the image will be modified in place. Otherwise, a new image will be created.
+        :type inplace: bool
         :return: The image with the face detected.
         :rtype: DrowsinessImage
         """
